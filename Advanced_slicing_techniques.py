87d0a5a6df8e9e6e3e090c31f30e84f3fdd3a927
jrdaiv <jrdaiv@gmail.com>
2024-06-12 19:52:58 -0700

    Assignment 1

---
+++ Advanced_slicing_techniques.py
@@ -0,0 +1,37 @@
+# Task 1
+
+temperatures = [72, 75, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106]
+second_week = (temperatures[7:14])
+print(f"Second week temps: {second_week}")
+
+
+# Task 2
+
+# temps = temperatures[24:30]
+
+temperatures = [72, 75, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106]
+high_temps = (temperatures[24:])
+print(f"High temps are from 100-106! {high_temps}")
+
+
+# Task 3
+
+temperatures = [72, 75, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106]
+low_temps = (temperatures[4:10])
+print(f"Temps from the 5th to the 10th: {low_temps}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Python_List_Transformation.py
@@ -0,0 +1,20 @@
+# Task 1
+
+# sort by descending list
+
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+grades.sort(reverse = True)
+print(grades)
+
+
+# Task 2
+
+percents = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+average_grade = sum(percents) / len(percents)
+print("average_grade:", average_grade)
+
+
+
+
+
+
